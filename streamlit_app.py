8266345ebdef3da979235ac254c16e6c70f1d841
marl-Am <marl-Am@users.noreply.github.com>
2025-05-29 09:57:09 -0400

    Added a button to download a sample Excel template

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,10 @@ for folder in [UPLOAD_DIR, DATA_DIR, OUTPUT_DIR, REPORT_DIR]:
 st.set_page_config(page_title="Inventory Tracker", layout="wide")
 st.title("Inventory & Revenue Dashboard")
 
-uploaded_file = st.file_uploader("Upload your inventory data", type=["xlsx", "xls"])
+uploaded_file = st.file_uploader("Upload your excel file", type=["xlsx", "xls"])
+st.markdown("### Don't have a file?")
+with open("sample.xlsx", "rb") as f:
+    st.download_button("Download Sample Excel Template", f, file_name="sample.xlsx")
 
 if uploaded_file:
     file_path = os.path.join(UPLOAD_DIR, uploaded_file.name)
